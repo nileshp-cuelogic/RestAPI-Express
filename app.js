0892ee47066dedd6920d9e680f751732566f397f
nilesh.patil@cuelogic.co.in <nilesh.patil@cuelogic.co.in>
2017-05-26 18:13:11 +0530

    joi validation and cors

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+
 var express = require('express');
 var app = express();
+
+
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/UserManagement");
@@ -8,4 +11,5 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
     console.log('Connected To Mongo Database');
 });
+
 module.exports = app;
--- modules/user/validators/userValidator.js
+++ modules/user/validators/userValidator.js
@@ -1,42 +1,51 @@
+var Joi = require('joi');
 var UserValidator = {};
 var UserModel = require('./../model/userModel');
 
+var JOIUserIdValidation = Joi.number().greater(0);
+
 UserValidator.ValidateGetUserParameter = function (req, res, next) {
-    if (!req.params || !req.params.id || isNaN(req.params.id) || req.params.id < 0) {
-        return res.status(400).send({
-            message: 'Invalid User Id'
-        });
-    }
-    UserModel.find({
-        UserId: req.body.UserId
-    }, function (err, docs) {
-        if (!docs.length) {
+
+    Joi.validate(req.params.id, JOIUserIdValidation, function (err, val) {
+        if (err) {
             return res.status(400).send({
-                message: 'Record Not Found'
+                Error: err
             });
         } else {
-            next();
+            UserModel.find({
+                UserId: req.params.id
+            }, function (err, docs) {
+                if (!docs.length) {
+                    return res.status(400).send({
+                        message: 'Record Not Found'
+                    });
+                } else {
+                    next();
+                }
+            });
         }
     });
 };
 
 UserValidator.ValidateRemoveUserParameter = function (req, res, next) {
 
-    if (!req.body || !req.body.UserId || isNaN(req.body.UserId) || req.body.UserId < 0) {
-        return res.status(400).send({
-            message: 'Invalid User Id'
-        });
-    }
-
-    UserModel.find({
-        UserId: req.body.UserId
-    }, function (err, docs) {
-        if (!docs.length) {
+    Joi.validate(req.body.UserId, JOIUserIdValidation, function (err, val) {
+        if (err) {
             return res.status(400).send({
-                message: 'Record Not Found'
+                Error: err
             });
         } else {
-            next();
+            UserModel.find({
+                UserId: req.body.UserId
+            }, function (err, docs) {
+                if (!docs.length) {
+                    return res.status(400).send({
+                        message: 'Record Not Found'
+                    });
+                } else {
+                    next();
+                }
+            });
         }
     });
 };
